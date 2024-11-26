af945034e88603d2e5f3c6579040d979704e23e2
SofiaMiyata <162508624+SofiaMiyata@users.noreply.github.com>
2024-11-26 14:15:16 -0300

    Update ledstop.py

--- ledstop.py
+++ ledstop.py
@@ -1,7 +1,7 @@
-from gpiozero import LED
-from time import sleep
+from gpiozero import LED #importa o pwm da biblioteca gpiozero
+from time import sleep #Importa a função sleep da biblioteca time para criar a base de tempo 
 
-pin = LED(18)
+pin = LED(18) #Define-se o pino do led como 18
 
-while True:
-	pin.on()
+while True: #Loop infinito
+	pin.on()  #Permanece com o led aceso após pausar o serviço
