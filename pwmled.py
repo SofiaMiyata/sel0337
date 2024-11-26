7a3d2246473d930113b64c1a27c8a4ffb8511c7e
SofiaMiyata <162508624+SofiaMiyata@users.noreply.github.com>
2024-11-26 13:45:26 -0300

    Update pwmled.py

--- pwmled.py
+++ pwmled.py
@@ -1,14 +1,14 @@
-from gpiozero import PWMLED
-from time import sleep
+from gpiozero import PWMLED #importa o pwm da biblioteca gpiozero
+from time import sleep  #Importa a função sleep da biblioteca time para criar a base de tempo 
 
-led = PWMLED(18)
+led = PWMLED(18) 		#funcao que permite controlar o LED por PWM(com valores entre 0 e 1)
 
-while True:
-	led.value = 0
-	sleep(0.5)
-	led.value = 0.25
-	sleep(0.5)
-	led.value = 0.5
-	sleep(0.5)
-	led.value = 1
-	sleep(0.5)
+while True: # loop infinito
+	led.value = 0 		# brilho do LED em 0%
+	sleep(0.5)    		# brilho em 0% durante 0,5 segundos
+	led.value = 0.25	#brilho em 25%
+	sleep(0.5)			# brilho em 25% durante 0,5 segundos
+	led.value = 0.5		#brilho em 50%
+	sleep(0.5)			# brilho em 50% durante 0,5 segundos
+	led.value = 1	     #brilho em 100%
+	sleep(0.5)			# brilho em 100% durante 0,5 segundos
